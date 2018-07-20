6a1fd0c856f1f19af2104118639ac78604aa3a81
Adam Cohn <adamcohn88@gmail.com>
2018-07-20 13:16:53 -0400

    working on table

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,20 +1,54 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { Table } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const tableData = [
-  { name: 'John', age: 15, gender: 'Male' },
-  { name: 'Amber', age: 40, gender: 'Female' },
-  { name: 'Leslie', age: 25, gender: 'Female' },
-  { name: 'Ben', age: 70, gender: 'Male' },
-];
-
-export default class ResultsTable extends Component {
+class ResultsTable extends Component {
   state = {
     column: null,
-    data: tableData,
+    data: [],
     direction: null,
   };
+
+  componentDidMount() {
+    this.formatData();
+  }
+
+  formatData = () => {
+    const data = [];
+    this.props.results.forEach(county => {
+      const trump = county.candidates.filter(
+        candidate => candidate['normalized_name'] === 'trump',
+      )[0].results;
+      const clinton = county.candidates.filter(
+        candidate => candidate['normalized_name'] === 'clinton',
+      )[0].results;
+      const johnson = county.candidates.filter(
+        candidate => candidate['normalized_name'] === 'johnson',
+      )[0].results;
+      const stein = county.candidates.filter(
+        candidate => candidate['normalized_name'] === 'stein',
+      )[0].results;
+      // const mcmullin = county.candidates.filter(
+      //   candidate => candidate['normalized_name'] === 'mcmullin',
+      // )[0].results;
+      const other = county.candidates.filter(
+        candidate => candidate['normalized_name'] === 'other',
+      )[0].results;
+      data.push({
+        name: county.county_name,
+        trump: trump,
+        clinton: clinton,
+        johnson: johnson,
+        stein: stein,
+        // mcmullin: mcmullin,
+        other: other,
+      });
+    });
+    this.setState({ data: data });
+  };
+
+  //{county: '', trump: '', clinton: '', other: ''}
 
   handleSort = clickedColumn => () => {
     const { column, data, direction } = this.state;
@@ -25,7 +59,6 @@ export default class ResultsTable extends Component {
         data: _.sortBy(data, [clickedColumn]),
         direction: 'ascending',
       });
-
       return;
     }
 
@@ -39,39 +72,44 @@ export default class ResultsTable extends Component {
     const { column, data, direction } = this.state;
 
     return (
-      <Table sortable celled fixed>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell
-              sorted={column === 'name' ? direction : null}
-              onClick={this.handleSort('name')}
-            >
-              Name
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'age' ? direction : null}
-              onClick={this.handleSort('age')}
-            >
-              Age
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'gender' ? direction : null}
-              onClick={this.handleSort('gender')}
-            >
-              Gender
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {_.map(data, ({ age, gender, name }) => (
-            <Table.Row key={name}>
-              <Table.Cell>{name}</Table.Cell>
-              <Table.Cell>{age}</Table.Cell>
-              <Table.Cell>{gender}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+      <div>
+        {this.props.results.length > 0 && (
+          <Table sortable celled fixed>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell
+                  sorted={column === 'county_name' ? direction : null}
+                  onClick={this.handleSort('county_name')}
+                >
+                  County
+                </Table.HeaderCell>
+                {this.props.results[0].candidates.map(candidate => (
+                  <Table.HeaderCell
+                    sorted={column === candidate.normalized_name ? direction : null}
+                    onClick={this.handleSort(candidate.normalized_name)}
+                  >
+                    {candidate.name}
+                  </Table.HeaderCell>
+                ))}
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {data.map({county} => (
+                <Table.Row key={county.fips}>
+                  <Table.Cell>{county.county_name}</Table.Cell>
+                  <Table.Cell>{county.candidates.normalized_name}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        )}
+      </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  results: state.results.electionResults,
+});
+
+export default connect(mapStateToProps)(ResultsTable);
