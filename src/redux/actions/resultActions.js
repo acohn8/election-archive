9201dd53b3a043d0da2f7424c533ac041b2bb7a4
Adam Cohn <adamcohn88@gmail.com>
2018-07-20 22:45:52 -0400

    started building candidates

--- src/redux/actions/resultActions.js
+++ src/redux/actions/resultActions.js
@@ -2,8 +2,27 @@ const fetchResults = () => (dispatch) => {
   dispatch({ type: 'LOADING' });
   return fetch('http://localhost:3000/api/v1/states/3/counties')
     .then(res => res.json())
-    .then(countyData => dispatch(countyResults(countyData)));
+    .then((countyData) => {
+      // fetchCandidateDetails(countyData.counties[0].results[0]);
+      dispatch(countyResults(countyData));
+    });
 };
+
+// const fetchCandidateDetails = (resultsList) => {
+//   fetch('http://localhost:3000/api/v1/candidates')
+//     .then(res => res.json())
+//     .then((candidateInfo) => {
+//       const candidates = Object.keys(resultsList);
+//       const found = candidateInfo.data.filter(fetchedCandidates =>
+//         candidateInfo.includes(fetchedCandidates.id))
+//       console.log(found);
+//     });
+// };
+
+const candidateInfo = (candidates) => {
+  console.log(candidates);
+};
+
 const countyResults = response => ({
   type: 'COUNTY_RESULTS',
   stateName: response.state,
