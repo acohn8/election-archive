e570197fb62c1fff937399dc849283268a65e69e
Adam Cohn <adamcohn88@gmail.com>
2018-07-24 15:45:44 -0400

    fix bug

--- src/redux/actions/stateActions.js
+++ src/redux/actions/stateActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { fetchStateData } from './resultActions';
 
 const fetchStatesList = () => async (dispatch) => {
-  const response = await axios.get('https://election-data-2016.herokuapp.com/api/v1');
+  const response = await axios.get('https://election-data-2016.herokuapp.com/api/v1/states');
   dispatch({ type: 'SET_STATES', states: response.data.data });
 };
 
