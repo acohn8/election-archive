a667978b010dd7d54dbd43e4fda726909ddb320c
Adam Cohn <adamcohn88@gmail.com>
2018-07-25 13:57:49 -0400

    table working!

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,33 +1,56 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 import { connect } from 'react-redux';
+import { div } from 'gl-matrix/src/gl-matrix/vec4';
 
-const ResultsTable = props => (
-  <Table sortable celled fixed selectable>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Name</Table.HeaderCell>
-        {Object.keys(props.electionResults.entities.results[props.electionResults.result[0]].results).map(candidateId => (
-          <Table.HeaderCell key={candidateId}>
-            {candidateId === 'other'
-              ? 'Other'
-              : props.candidates.entities.candidates[candidateId].attributes.name}
-          </Table.HeaderCell>
-        ))}
-      </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      {props.electionResults.result.map(countyId => (
-        <Table.Row key={countyId}>
-          <Table.Cell>{props.geography.entities.counties[countyId].name}</Table.Cell>
-          {Object.values(props.electionResults.entities.results[countyId].results).map(result => (
-            <Table.Cell key={result}>{result.toLocaleString()}</Table.Cell>
-          ))}
-        </Table.Row>
-      ))}
-    </Table.Body>
-  </Table>
-);
+const ResultsTable = (props) => {
+  const majorCandidates = Object.keys(props.electionResults.entities.results[props.electionResults.result[0]].results);
+
+  const data = props.electionResults.result.map(countyId => ({
+    countyId,
+    county: props.geography.entities.counties[countyId].name,
+  }));
+
+  data.forEach((county) => {
+    county.candidates = {};
+    majorCandidates.forEach((candidateId) => {
+      county.candidates[candidateId] = {
+        votes: props.electionResults.entities.results[county.countyId].results[candidateId],
+      };
+    });
+  });
+
+  const columns = [
+    {
+      Header: 'County',
+      id: 'county',
+      accessor: d => d.county,
+    },
+  ];
+
+  majorCandidates.map((candidateId) => {
+    columns.push({
+      id: candidateId,
+      Header: `${
+        candidateId === 'other'
+          ? 'Other'
+          : props.candidates.entities.candidates[candidateId].attributes.name
+      }`,
+      accessor: d => d.candidates[candidateId].votes,
+    });
+  });
+
+  return (
+    <ReactTable
+      data={data}
+      columns={columns}
+      defaultPageSize={10}
+      className="-striped -highlight"
+    />
+  );
+};
 
 const mapStateToProps = state => ({
   candidates: state.results.candidates,
