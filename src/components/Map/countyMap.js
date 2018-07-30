b71d03c07735f6aa7e9511f7dbfbcd1aad5dd1df
Adam Cohn <adamcohn88@gmail.com>
2018-07-30 09:25:07 -0400

    new county maps

--- src/components/Map/countyMap.js
+++ src/components/Map/countyMap.js
@@ -23,7 +23,6 @@ class CountyMap extends React.Component {
       container: this.mapContainer,
       style: 'mapbox://styles/adamcohn/cjjyfk3es0nfj2rqpf9j53505',
       zoom: this.props.mapDetails.zoom,
-      //grabs the lat long from the first county in the state to ensure the counties layer is loading the right geos
       center: this.props.mapDetails.center,
     });
 
@@ -32,83 +31,74 @@ class CountyMap extends React.Component {
     });
   };
 
-  makeDataLayer = () => {
-    let demCandidate;
-    let gopCandidate;
-    findTopCandidates(this.props.candidates, this.props.electionResults).forEach(candidateId => {
-      if (
-        this.props.candidates.entities.candidates[candidateId].attributes.party === 'democratic'
-      ) {
-        demCandidate = candidateId;
-      } else if (
-        this.props.candidates.entities.candidates[candidateId].attributes.party === 'republican'
-      ) {
-        gopCandidate = candidateId;
-      }
+  addResultsLayer = () => {
+    this.map.addSource('presResults', {
+      url: 'mapbox://adamcohn.019zfefq',
+      type: 'vector',
     });
-    //finds the county layer, filters counties with matching fips, matches results from state
-    const fips = this.props.geography.entities.counties[this.props.precinctResults.county_id].fips;
-    const stateCounties = this.map
+
+    this.map.addLayer(
+      {
+        id: 'dem-margin',
+        type: 'fill',
+        source: 'presResults',
+        'source-layer': '2016_pres-asczpp',
+        filter: [
+          '==',
+          ['get', 'GEOID'],
+          this.props.geography.entities.counties[this.props.precinctResults.county_id].fips
+            .toString()
+            .padStart(2, '0'),
+        ],
+
+        paint: {
+          'fill-color': [
+            'interpolate',
+            ['linear'],
+            ['get', 'county_r_2'],
+            -0.3,
+            '#d6604d',
+            -0.2,
+            '#f4a582',
+            -0.1,
+            '#fddbc7',
+            0.0,
+            '#f7f7f7',
+            0.1,
+            '#d1e5f0',
+            0.2,
+            '#92c5de',
+            0.3,
+            '#4393c3',
+          ],
+          'fill-opacity': 0.7,
+        },
+      },
+      'waterway-label',
+    );
+
+    const mapFeatures = this.map
       .querySourceFeatures('composite', {
         sourceLayer: 'cb_2017_us_county_500k-7qwbcn',
       })
-      .slice()
-      .filter(county => fips === parseInt(county.properties.GEOID, 0));
-
-    const countyResults = this.props.electionResults.result.map(countyId => ({
-      fips: this.props.geography.entities.counties[this.props.precinctResults.county_id].fips
-        .toString()
-        .padStart(5, '0'),
-      results: this.props.electionResults.entities.results[this.props.precinctResults.county_id]
-        .results,
-    }));
-
-    stateCounties.forEach(county => {
-      const result = countyResults.find(
-        countyResult => countyResult.fips === county.properties.GEOID,
+      .filter(
+        county =>
+          county.properties.GEOID ===
+          this.props.geography.entities.counties[this.props.precinctResults.county_id].fips
+            .toString()
+            .padStart(5, '0'),
       );
-      const demMargin = parseFloat(
-        (result.results[demCandidate] - result.results[gopCandidate]) /
-          (result.results[demCandidate] + result.results[gopCandidate]),
-      );
-      const demVotes = result.results[demCandidate];
-      const gopVotes = result.results[gopCandidate];
-      stateCounties[stateCounties.indexOf(county)].properties.demMargin = demMargin;
-      stateCounties[stateCounties.indexOf(county)].properties.demVotes = demVotes;
-      stateCounties[stateCounties.indexOf(county)].properties.gopVotes = gopVotes;
-    });
-    return stateCounties;
-  };
 
-  addResultsLayer = () => {
-    this.map.addSource('results', {
+    this.map.addSource('counties', {
       type: 'geojson',
       data: {
         type: 'FeatureCollection',
-        features: this.makeDataLayer(),
+        features: mapFeatures,
       },
     });
-    this.map.addLayer({
-      id: 'dem-margin',
-      type: 'fill',
-      source: 'results',
-      paint: {
-        'fill-color': [
-          'interpolate',
-          ['linear'],
-          ['get', 'demMargin'],
-          -0.3,
-          '#ef8a62',
-          0,
-          '#f7f7f7',
-          0.3,
-          '#67a9cf',
-        ],
-        'fill-opacity': 1,
-      },
-    });
+
     this.map.moveLayer('dem-margin', 'poi-parks-scalerank2');
-    const boundingBox = bbox(this.map.getSource('results')._data);
+    const boundingBox = bbox(this.map.getSource('counties')._data);
     this.map.fitBounds(boundingBox, { padding: 70, animation: { duration: 200 } });
   };
 
