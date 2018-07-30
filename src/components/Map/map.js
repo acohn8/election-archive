fd703d89d36e743835bb56275e4a8330636ae644
Adam Cohn <adamcohn88@gmail.com>
2018-07-29 22:19:46 -0400

    vector maps

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -48,7 +48,7 @@ class Map extends React.Component {
 
     this.map.on('load', () => {
       this.addResultsLayer();
-      this.enableHover();
+      // this.enableHover();
     });
   };
 
@@ -80,111 +80,68 @@ class Map extends React.Component {
     });
   };
 
-  makeDataLayer = () => {
-    let demCandidate;
-    let gopCandidate;
-    findTopCandidates(this.props.candidates, this.props.electionResults).forEach(candidateId => {
-      if (
-        this.props.candidates.entities.candidates[candidateId].attributes.party === 'democratic'
-      ) {
-        demCandidate = candidateId;
-      } else if (
-        this.props.candidates.entities.candidates[candidateId].attributes.party === 'republican'
-      ) {
-        gopCandidate = candidateId;
-      }
-    });
-    //finds the county layer, filters counties with matching fips, matches results from state
-    const stateCounties = this.map
-      .querySourceFeatures('composite', {
-        sourceLayer: 'cb_2017_us_county_500k-7qwbcn',
-      })
-      .slice()
-      .filter(
-        feature =>
-          parseInt(feature.properties.STATEFP, 0) ===
-          this.props.geography.entities.state[this.props.states.activeStateId].fips,
-      );
-    const countyResults = this.props.electionResults.result.map(countyId => ({
-      fips: this.props.geography.entities.counties[countyId].fips.toString().padStart(5, '0'),
-      results: this.props.electionResults.entities.results[countyId].results,
-    }));
-    stateCounties.forEach(county => {
-      const result = countyResults.find(
-        countyResult => countyResult.fips === county.properties.GEOID,
-      );
-      console.log(county, result);
-      const demMargin = parseFloat(
-        (result.results[demCandidate] - result.results[gopCandidate]) /
-          (result.results[demCandidate] + result.results[gopCandidate]),
-      );
-      const demVotes = result.results[demCandidate];
-      const gopVotes = result.results[gopCandidate];
-      stateCounties[stateCounties.indexOf(county)].properties.demMargin = demMargin;
-      stateCounties[stateCounties.indexOf(county)].properties.demVotes = demVotes;
-      stateCounties[stateCounties.indexOf(county)].properties.gopVotes = gopVotes;
-    });
-    return stateCounties;
-  };
-
   addResultsLayer = () => {
-    //   this.map.addSource('countyLines', {
-    //     url: 'mapbox://adamcohn.4rxuwfht',
-    //     type: 'vector',
-    //   });
+    this.map.addSource('presResults', {
+      url: 'mapbox://adamcohn.019zfefq',
+      type: 'vector',
+    });
 
-    //   this.map.addLayer(
-    //     {
-    //       id: 'states-join',
-    //       type: 'line',
-    //       source: 'countyLines',
-    //       'source-layer': 'cb_2017_us_county_5m-2n1v3o',
-    //       filter: [
-    //         '==',
-    //         'STATEFP',
-    //         this.props.geography.entities.state[this.props.geography.result.state].fips,
-    //       ],
-    //       paint: {
-    //         'line-color': '#ff69b4',
-    //         'line-width': 1,
-    //       },
-    //     },
-    //     'waterway-label',
-    //   );
+    this.map.addLayer(
+      {
+        id: 'dem-margin',
+        type: 'fill',
+        source: 'presResults',
+        'source-layer': '2016_pres-asczpp',
+        filter: [
+          '==',
+          ['get', 'STATEFP'],
+          this.props.geography.entities.state[this.props.geography.result.state].fips
+            .toString()
+            .padStart(2, '0'),
+        ],
+        paint: {
+          'fill-color': [
+            'interpolate',
+            ['linear'],
+            ['get', 'county_r_2'],
+            -0.3,
+            '#ef8a62',
+            0,
+            '#f7f7f7',
+            0.3,
+            '#67a9cf',
+          ],
+          'fill-opacity': 0.7,
+        },
+      },
+      'waterway-label',
+    );
 
-    this.map.addSource('results', {
+    this.map.addSource('counties', {
       type: 'geojson',
       data: {
         type: 'FeatureCollection',
-        features: this.makeDataLayer(),
+        features: this.map
+          .querySourceFeatures('composite', {
+            sourceLayer: 'cb_2017_us_county_500k-7qwbcn',
+          })
+          .filter(
+            county =>
+              county.properties.STATEFP ===
+              this.props.geography.entities.state[this.props.geography.result.state].fips
+                .toString()
+                .padStart(2, '0'),
+          ),
       },
     });
-    this.map.addLayer({
-      id: 'dem-margin',
-      type: 'fill',
-      source: 'results',
-      paint: {
-        'fill-color': [
-          'interpolate',
-          ['linear'],
-          ['get', 'demMargin'],
-          -0.3,
-          '#ef8a62',
-          0,
-          '#f7f7f7',
-          0.3,
-          '#67a9cf',
-        ],
-        'fill-opacity': 1,
-      },
-    });
-    const boundingBox = bbox(this.map.getSource('results')._data);
-    this.map.fitBounds(boundingBox, { padding: 10, animate: false });
+    console.log(this.map.getSource('counties'));
+    // const boundingBox = bbox(this.map.getSource('counties')._data);
+    // this.map.fitBounds(this.map.getSource('counties').bounds);
     this.map.moveLayer('dem-margin', 'poi-parks-scalerank2');
     const mapDetails = {
       center: this.map.getCenter(),
       zoom: this.map.getZoom(),
-      bbox: boundingBox,
+      // bbox: boundingBox,
     };
     this.props.setMapDetails(mapDetails);
   };
@@ -216,3 +173,31 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps,
 )(Map);
+
+// addResultsLayer = () => {
+//   this.map.addSource('mn-votes', {
+//     type: 'vector',
+//     url: 'mapbox://adamcohn.5lxa6wia',
+//   });
+
+//   this.map.addLayer({
+//     id: 'clinton-votes',
+//     source: 'mn-votes',
+//     'source-layer': 'MN-2016-a7m9cx',
+//     type: 'fill',
+//     paint: {
+//       'fill-color': [
+//         'interpolate',
+//         ['linear'],
+//         ['/', ['get', 'USPRSDFL'], ['get', 'USPRSTOTAL']],
+//         0,
+//         'red',
+//         0.5,
+//         'white',
+//         1,
+//         'blue',
+//       ],
+//       'fill-opacity': 0.75,
+//     },
+//   });
+// };
