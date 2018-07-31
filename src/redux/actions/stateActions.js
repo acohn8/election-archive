6cfe9a6d62c7342cdb2466da4e3695c60c61f996
Adam Cohn <adamcohn88@gmail.com>
2018-07-31 11:34:45 -0400

    pushing to heroku

--- src/redux/actions/stateActions.js
+++ src/redux/actions/stateActions.js
@@ -5,7 +5,7 @@ import fetchStateData from './resultActions';
 import { resetHover } from './mapActions';
 
 const fetchStatesList = () => async (dispatch) => {
-  const response = await axios.get('http://localhost:3000/api/v1/states');
+  const response = await axios.get('https://election-data-2016.herokuapp.com/api/v1/states');
   dispatch({ type: 'SET_STATES', states: response.data.data });
 };
 
