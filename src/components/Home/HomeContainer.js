cca80ddbe3f3d817ef6079a1a04d1ae4a22704cc
Adam Cohn <adamcohn88@gmail.com>
2018-08-02 14:25:52 -0400

    fixes

--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Container, Grid, Divider } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import NationalMap from './NationalMap';
 import MapInfo from '../Map/mapInfo';
