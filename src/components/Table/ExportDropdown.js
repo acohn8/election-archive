c97dae6a6e1a7cda5b0d62a049f2d02cd976c8a7
Adam Cohn <adamcohn88@gmail.com>
2018-08-15 11:12:21 -0400

    exports for all races

--- src/components/Table/ExportDropdown.js
+++ src/components/Table/ExportDropdown.js
@@ -2,32 +2,41 @@ import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
-const ExportDropdown = props => (
-  <Dropdown text="CSV Export" icon="cloud download" floating labeled button className="icon">
-    <Dropdown.Menu>
-      <Dropdown.Header icon="globe" content="Select a geography" />
-      <Dropdown.Item
-        as="a"
-        href={`https://s3.amazonaws.com/stateprecinctresults/countyresults/${props.geography.entities.state[
-          props.states.activeStateId
-        ].short_name.toLowerCase()}-county-results.csv`}
-      >
-        County
-      </Dropdown.Item>
-      <Dropdown.Item
-        as="a"
-        href={`https://s3.amazonaws.com/stateprecinctresults/precinctresults/${props.geography.entities.state[
-          props.states.activeStateId
-        ].short_name.toLowerCase()}-precinct-results.csv`}
-      >
-        Precinct
-      </Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-);
+const ExportDropdown = (props) => {
+  const stateName = props.geography.entities.state[
+    props.states.activeStateId
+  ].short_name.toLowerCase();
+
+  const officeName = props.offices.offices
+    .find(office => office.id === props.offices.selectedOfficeId.toString())
+    .attributes.name.toLowerCase()
+    .split(' ')
+    .join('_');
+
+  return (
+    <Dropdown text="CSV Export" icon="cloud download" floating labeled button className="icon">
+      <Dropdown.Menu>
+        <Dropdown.Header icon="globe" content="Select a geography" />
+        <Dropdown.Item
+          as="a"
+          href={`https://s3.amazonaws.com/stateprecinctresults/statewideexports/${stateName}/${officeName}/counties/${stateName}_${officeName}_county_results.csv`}
+        >
+          County
+        </Dropdown.Item>
+        <Dropdown.Item
+          as="a"
+          href={`https://s3.amazonaws.com/stateprecinctresults/statewideexports/${stateName}/${officeName}/precincts/${stateName}_${officeName}_precinct_results.csv`}
+        >
+          Precinct
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
 
 const mapStateToProps = state => ({
   states: state.states,
+  offices: state.offices,
   geography: state.results.geography,
 });
 
