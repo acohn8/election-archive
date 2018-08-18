d1d1a473172e4e9929a69828f20a680d8e0055fe
Adam Cohn <adamcohn88@gmail.com>
2018-08-17 20:44:40 -0400

    more mobile fixes

--- src/components/StateList/MobileStateSelect.js
+++ src/components/StateList/MobileStateSelect.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { Modal, Dropdown } from 'semantic-ui-react';
+import { Modal, Dropdown, List, Image } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
-import StateListContainer from './StateListContainer';
-
-const MobileStateSelector = ({ state }) => (
-  <Modal dimmer="blurring" trigger={<Dropdown text={state} />} closeIcon>
-    <Modal.Header>States</Modal.Header>
-    <Modal.Content scrolling>
-      <StateListContainer />
-    </Modal.Content>
-  </Modal>
-);
+const MobileStateSelector = (props) => {
+  const importAll = r => r.keys().map(r);
+  const images = importAll(require.context('../state-flags', false, /\.(png|jpe?g|svg)$/));
+  return (
+    <Modal dimmer="blurring" trigger={<Dropdown text={props.state} />} closeIcon>
+      <Modal.Header>States</Modal.Header>
+      <Modal.Content scrolling>
+        <List selection verticalAlign="middle">
+          {props.states.states.map(state => (
+            <List.Item
+              key={state.id}
+              href={`/states/${state.attributes.name
+                .split(' ')
+                .join('-')
+                .toLowerCase()}`}
+            >
+              <Image
+                avatar
+                src={images.find(image =>
+                  image.includes(`/static/media/${state.attributes['short-name'].toLowerCase()}`))}
+              />
+              <List.Content>
+                <List.Header>{state.attributes.name}</List.Header>
+              </List.Content>
+            </List.Item>
+          ))}
+        </List>
+      </Modal.Content>
+    </Modal>
+  );
+};
 
 const mapStateToProps = state => ({
   states: state.states,
