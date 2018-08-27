66797ecba8884a6eb26b2f02c456c14bc2a2def0
Adam Cohn <adamcohn88@gmail.com>
2018-08-27 12:12:54 -0400

    more small tweaks

--- src/components/Toplines/financeTable.js
+++ src/components/Toplines/financeTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Header } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 const CampaignFinanceTable = props => (
