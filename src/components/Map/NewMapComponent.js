f181de76ec12ae2284b5f81ee749f6fc689e644b
Adam Cohn <adamcohn88@gmail.com>
2018-08-27 23:50:02 -0400

    working on hover

--- src/components/Map/NewMapComponent.js
+++ src/components/Map/NewMapComponent.js
@@ -3,15 +3,17 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { connect } from 'react-redux';
 import bbox from '@turf/bbox';
+import union from '@turf/union';
 
 import { StateColorScale, CountyColorScale } from '../../functions/ColorScale';
 
-// import {
-//   getHoverInfo,
-//   setMapDetails,
-//   resetHover,
-//   resetMapDetails,
-// } from '../../redux/actions/mapActions';
+import {
+  getHoverInfo,
+  //   setMapDetails,
+  //   resetHover,
+  //   resetMapDetails,
+} from '../../redux/actions/mapActions';
+import { geometry } from '@turf/helpers';
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYWRhbWNvaG4iLCJhIjoiY2pod2Z5ZWQzMDBtZzNxcXNvaW8xcGNiNiJ9.fHYsK6UNzqknxKuchhfp7A';
@@ -20,22 +22,40 @@ class NewMap extends React.Component {
   componentDidMount() {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/adamcohn/cjjyfk3es0nfj2rqpf9j53505',
+      style: 'mapbox://styles/adamcohn/cjld3m3g66xlb2sl2ojc73n5v',
     });
   }
 
   componentDidUpdate() {
     if (this.props.allOffices.result !== undefined) {
-      const office = this.props.allOffices.entities.offices[this.props.selectedOffice];
+      const layers = [
+        {
+          name: 'county',
+          sourceLayer: 'cb_2017_us_county_500k',
+          colorScale: CountyColorScale,
+          layer: 'county-map',
+          filter: 'GEOID',
+          order: 1,
+        },
+        {
+          name: 'state',
+          sourceLayer: 'cb_2017_us_state_500k',
+          colorScale: StateColorScale,
+          layer: 'state-map',
+          filter: 'GEOID',
+          order: 2,
+        },
+      ];
       this.map.on('load', () => {
         this.map.on('load');
-        this.addSources('county', office);
-        this.addFillLayer('county');
-        this.addLineLayer('county');
-        this.addHoverLayer('county', 'GEOID');
-        this.setFilter(['countyFill', 'countyLines'], 'STATEFP', '12');
-        this.enableHover('county', 'GEOID');
-        this.bindToMap('county', 'STATEFP', '12');
+        this.addSources(layers);
+        this.addFillLayers(layers, 4.2);
+        this.addLineLayers(layers, 4.2);
+        this.addHoverLayers(layers);
+        // this.setFilter(['stateFill', 'countyFill', 'stateLine', 'countyLine'], 'STATEFP', '12');
+        this.enableHover(layers);
+        // this.bindToMap();
+        // this.stateSelection();
       });
     }
   }
@@ -44,56 +64,155 @@ class NewMap extends React.Component {
     this.map.remove();
   }
 
-  enableHover = (geography, filter) => {
-    this.map.on('mousemove', e => {
+  enableHover = geographies => {
+    geographies.forEach(geography =>
+      this.map.on('mousemove', `${geography.name}Fill`, e => {
+        if (e.features) {
+          const feature = e.features[0];
+          console.log(this.map.getSource('composite').vectorLayerIds);
+          const sourceFeatures = this.map.querySourceFeatures('composite', {
+            sourceLayer: geography.sourceLayer,
+            filter: ['==', geography.filter, feature.properties[geography.filter]],
+          });
+          // const joinedFeatures = union.apply(this, sourceFeatures);
+          // console.log(joinedFeatures);
+          // const layer = sourceFeatures.map(sourceFeature => {
+          //   const areaFeature = sourceFeature[0];
+          //   union(sourceFeature, areaFeature);
+          // });
+          this.map.getSource(`${geography.name}Hover`).setData({
+            type: 'FeatureCollection',
+            features: sourceFeatures,
+          });
+          this.map.getCanvas().style.cursor = 'pointer';
+
+          this.props.getHoverInfo(
+            feature.properties.NAME,
+            feature.properties.winner_name,
+            feature.properties.winner_party,
+            feature.properties.winner_margin,
+            feature.properties.winner_votes,
+            feature.properties.second_name,
+            feature.properties.second_party,
+            feature.properties.second_margin,
+            feature.properties.second_votes,
+            true,
+          );
+        } else if (!e.features) {
+          this.map.getCanvas().style.cursor = '';
+          this.map.getSource(`${geography.name}Hover`).setData({
+            type: 'FeatureCollection',
+            features: [],
+          });
+          return;
+        }
+      }),
+    );
+  };
+
+  stateSelection = () => {
+    this.map.on('click', e => {
       const features = this.map.queryRenderedFeatures(e.point, {
-        layers: [`${geography}Fill`],
+        layers:
+          this.props.offices.selectedOfficeId !== '322'
+            ? ['dem-statewide-margin']
+            : ['dem-county-margin'],
       });
-      if (features.length > 0) {
-        const feature = features[0];
-        this.map.setFilter(`${geography}Hover`, ['==', filter, feature.properties[filter]]);
-        this.map.getCanvas().style.cursor = 'pointer';
-      } else if (features.length === 0) {
-        this.map.getCanvas().style.cursor = '';
-        this.map.setFilter(`${geography}Hover`, ['==', filter, '']);
+      if (features.length) {
+        const coords = e.lngLat;
+        const state = this.getStateName(features[0]);
+        const district = this.getDistrictId(features[0]);
+        if (district !== 0) {
+          this.setStateOnClick(state, coords, district);
+        }
       }
     });
   };
 
-  addSources = (geography, office) => {
-    this.map.addSource(`${geography}`, {
-      url: `mapbox://adamcohn.${office.attributes['county-map']}`,
-      type: 'vector',
-    });
+  getDistrictId = feature => {
+    if (this.props.offices.selectedOfficeId === '322') {
+      return feature.properties.id;
+    } else {
+      return null;
+    }
   };
 
-  addFillLayer = geography => {
-    this.map.addLayer(
-      {
-        id: `${geography}Fill`,
-        type: 'fill',
-        source: `${geography}`,
-        'source-layer': 'cb_2017_us_county_500k',
-        paint: CountyColorScale,
-      },
-      'waterway-label',
+  getStateName = feature => {
+    if (this.props.offices.selectedOfficeId === '322') {
+      return this.props.states.states.find(
+        state =>
+          state.attributes['short-name'].toLowerCase() ===
+          feature.properties.NAME.split('-')[0].toLowerCase(),
+      );
+    } else {
+      return this.props.states.states.find(
+        state =>
+          state.attributes['short-name'].toLowerCase() === feature.properties.STUSPS.toLowerCase(),
+      );
+    }
+  };
+
+  setStateOnClick = (state, coords, districtId) => {
+    this.props.setStateId(state.id);
+    this.props.fetchStateOffices(state.id);
+    this.props.setActiveOffice(this.props.offices.selectedOfficeId, districtId);
+    this.props.fetchStateData(state.id, districtId);
+    this.map.flyTo({
+      center: coords,
+      zoom: 6,
+      speed: 0.75,
+    });
+    this.map.on('moveend', () => this.props.pushToNewState(state.id));
+  };
+
+  addSources = geographies => {
+    geographies.forEach(geography =>
+      this.map.addSource(`${geography.name}`, {
+        url: `mapbox://adamcohn.${
+          this.props.allOffices.entities.offices[this.props.selectedOffice].attributes[
+            geography.layer
+          ]
+        }`,
+        type: 'vector',
+      }),
     );
   };
 
-  addLineLayer = geography => {
-    this.map.addLayer(
-      {
-        id: `${geography}Lines`,
-        type: 'line',
-        source: `${geography}`,
-        'source-layer': 'cb_2017_us_county_500k',
-        paint: {
-          'line-width': 0.3,
-          'line-color': '#696969',
-          'line-opacity': 0.6,
+  addFillLayers = (geographies, zoomThreshold = 0) => {
+    geographies.forEach(geography =>
+      this.map.addLayer(
+        {
+          id: `${geography.name}Fill`,
+          minzoom: geographies.indexOf(geography) === 0 ? zoomThreshold : 0,
+          maxzoom: geographies.indexOf(geography) === 1 ? zoomThreshold : 0,
+          type: 'fill',
+          source: geography.name,
+          'source-layer': geography.sourceLayer,
+          paint: geography.colorScale,
         },
-      },
-      'waterway-label',
+        'waterway-label',
+      ),
+    );
+  };
+
+  addLineLayers = (geographies, zoomThreshold = 0) => {
+    geographies.sort((a, b) => a.order - b.order).forEach(geography =>
+      this.map.addLayer(
+        {
+          id: `${geography.name}Line`,
+          minzoom: geographies.indexOf(geography) === 0 ? zoomThreshold : 0,
+          maxzoom: geographies.indexOf(geography) === 1 ? zoomThreshold : 0,
+          type: 'line',
+          source: geography.name,
+          'source-layer': geography.sourceLayer,
+          paint: {
+            'line-width': 0.3,
+            'line-color': '#696969',
+            'line-opacity': 0.6,
+          },
+        },
+        'waterway-label',
+      ),
     );
   };
 
@@ -101,37 +220,53 @@ class NewMap extends React.Component {
     layers.forEach(layer => this.map.setFilter(layer, ['==', property, value]));
   }
 
-  addHoverLayer = (geography, filter) => {
-    this.map.addLayer(
-      {
-        id: `${geography}Hover`,
+  addHoverLayers = geographies => {
+    geographies.forEach(geography =>
+      this.map.addSource(`${geography.name}Hover`, {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [],
+        },
+      }),
+    );
+
+    geographies.forEach(geography =>
+      this.map.addLayer({
+        id: `${geography.name}Hover`,
+        source: `${geography.name}Hover`,
         type: 'line',
-        source: `${geography}`,
-        'source-layer': 'cb_2017_us_county_500k',
-        filter: ['==', `${filter}`, ''],
         paint: {
           'line-width': 2,
           'line-color': '#696969',
           'line-opacity': 1,
         },
-      },
-      'waterway-label',
+      }),
     );
   };
 
-  bindToMap = (geography, property, value) => {
-    this.map.addSource(`${geography}-bounds`, {
+  filterGeojson = (property = null, value = null) => {
+    const layer = this.map.querySourceFeatures('composite', {
+      sourceLayer: '2016_county_results-5wvgz3',
+    });
+    if (property === null && value === null) {
+      return layer.filter(
+        geo => geo.properties.STATEFP !== '15' && geo.properties.STATEFP !== '02',
+      );
+    } else {
+      return layer.filter(geo => geo.properties[property] === value);
+    }
+  };
+
+  bindToMap = (property = null, value = null) => {
+    this.map.addSource('bounds', {
       type: 'geojson',
       data: {
         type: 'FeatureCollection',
-        features: this.map
-          .queryRenderedFeatures(() => {
-            `${geography}Fill}`;
-          })
-          .filter(geo => geo.properties[property] === value),
+        features: this.filterGeojson(property, value),
       },
     });
-    const boundingBox = bbox(this.map.getSource(`${geography}-bounds`)._data);
+    const boundingBox = bbox(this.map.getSource('bounds')._data);
     this.map.fitBounds(boundingBox, { padding: 20, animate: false });
   };
 
@@ -146,9 +281,41 @@ class NewMap extends React.Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  getHoverInfo: (
+    geographyName,
+    winnerName,
+    winnerParty,
+    winnerMargin,
+    winnerVotes,
+    secondName,
+    secondParty,
+    secondMargin,
+    secondVotes,
+    isNational,
+  ) =>
+    dispatch(
+      getHoverInfo(
+        geographyName,
+        winnerName,
+        winnerParty,
+        winnerMargin,
+        winnerVotes,
+        secondName,
+        secondParty,
+        secondMargin,
+        secondVotes,
+        isNational,
+      ),
+    ),
+});
+
 const mapStateToProps = state => ({
   allOffices: state.offices.allOffices,
   selectedOffice: state.offices.selectedOfficeId,
 });
 
-export default connect(mapStateToProps)(NewMap);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(NewMap);
--- src/components/Map/NewMapContainer.js
+++ src/components/Map/NewMapContainer.js
@@ -2,29 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import NewMap from './NewMapComponent';
+import MapInfo from './mapInfo';
 
 class NewMapContainer extends React.Component {
   render() {
     return (
       <div>
         <NewMap />
+        <div
+          style={{
+            position: 'absolute',
+            zIndex: 1,
+            left: 30,
+            borderRadius: '25px',
+            top: 80,
+            width: 300,
+            backgroundColor: 'white',
+            padding: '20px',
+            opacity: '0.8',
+            borderColor: 'gray',
+            borderStyle: 'solid',
+            borderWidth: '0.5px',
+          }}
+        >
+          <MapInfo />
+        </div>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({});
-
 const mapStateToProps = state => ({
-  // headerHid: state.maps.headerHid,
-  // overlay: state.maps.overlay,
   allOffices: state.offices.allOffices,
   selectedOffice: state.offices.selectedOfficeId,
-  // states: state.states.activeStateId,
-  // windowWidth: state.nav.windowWidth,
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(NewMapContainer);
+export default connect(mapStateToProps)(NewMapContainer);
