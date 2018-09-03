1d2c63edfea6400d686f6a78bd94bb4e3d233c70
Adam Cohn <adamcohn88@gmail.com>
2018-09-02 23:33:08 -0400

    more fixes

--- src/components/Toplines/FinanceOverview.js
+++ src/components/Toplines/FinanceOverview.js
@@ -7,36 +7,44 @@ const FinanceOverview = ({ campaignFinance }) => (
       <List.Icon name="dollar sign" size="large" verticalAlign="middle" />
       <List.Content>
         <List.Header>Receipts</List.Header>
-        {campaignFinance === undefined
-          ? 'Not available'
-          : `$${Math.round(campaignFinance.total_receipts).toLocaleString()}`}
+        <List.Description>
+          {campaignFinance === undefined
+            ? 'Not available'
+            : `$${Math.round(campaignFinance.total_receipts).toLocaleString()}`}
+        </List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
       <List.Icon name="payment" size="large" verticalAlign="middle" />
       <List.Content>
         <List.Header>Disbursements</List.Header>
-        {campaignFinance === undefined
-          ? 'Not available'
-          : `$${Math.round(campaignFinance.total_disbursements).toLocaleString()}`}
+        <List.Description>
+          {campaignFinance === undefined
+            ? 'Not available'
+            : `$${Math.round(campaignFinance.total_disbursements).toLocaleString()}`}
+        </List.Description>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name="money bill" size="large" verticalAlign="middle" />
+      <List.Content>
+        <List.Header>Ending Cash</List.Header>
+        <List.Description>
+          {campaignFinance === undefined
+            ? 'Not available'
+            : `$${Math.round(campaignFinance.end_cash).toLocaleString()}`}
+        </List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
       <List.Icon name="balance scale" size="large" verticalAlign="middle" />
       <List.Content>
-        <List.Header>Ending Cash</List.Header>
-        {campaignFinance === undefined
-          ? 'Not available'
-          : `$${Math.round(campaignFinance.end_cash).toLocaleString()}`}
-      </List.Content>
-    </List.Item>
-    <List.Item>
-      <List.Icon name="money bill alternate" size="large" verticalAlign="middle" />
-      <List.Content>
         <List.Header>Debts</List.Header>
-        {campaignFinance === undefined
-          ? 'Not available'
-          : `$${Math.round(campaignFinance.debts_owed).toLocaleString()}`}
+        <List.Description>
+          {campaignFinance === undefined
+            ? 'Not available'
+            : `$${Math.round(campaignFinance.debts_owed).toLocaleString()}`}
+        </List.Description>
       </List.Content>
     </List.Item>
   </List>
