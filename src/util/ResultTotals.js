f0e84d5a949f45e36271fdfd000c7cc16d2ab245
Adam Cohn <adamcohn88@gmail.com>
2018-09-15 19:56:34 -0400

    fixed aggregation issue

--- src/util/ResultTotals.js
+++ src/util/ResultTotals.js
@@ -2,7 +2,9 @@ const resultTotals = (data, total) => {
   const candidates = Object.keys(data[0].results);
   const totals = {};
   candidates.forEach((candidate) => {
-    const candidateResults = data.map(subGeo => subGeo.results[candidate]);
+    const candidateResults = data
+      .map(subGeo => subGeo.results[candidate])
+      .filter(geo => geo.total !== undefined);
     const voteTotal = candidateResults.map(result => result.total).reduce((sum, n) => sum + n);
     totals[candidate] = {};
     totals[candidate].votes = voteTotal;
